95d8a7731e4b9b686a3b78bd0e064ec75a622f1d
CedielRecords <86392021+cedielrecords@users.noreply.github.com>
2026-03-06 14:54:48 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const canvas = document.getElementById("matrix");
 const ctx = canvas.getContext("2d");
 
-function resizeCanvas(){
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-}
-
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas);
+let fontSize = 14;
+let columns;
+let drops = [];
 
 const letters = "未来都市建築設計速度光空間";
-const fontSize = 14;
 
-const columns = Math.floor(canvas.width / fontSize);
+function setup(){
 
-const drops = [];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+columns = Math.floor(canvas.width / fontSize);
+
+drops = [];
 
 for(let i = 0; i < columns; i++){
 drops[i] = Math.random() * canvas.height;
 }
+
+}
+
+window.addEventListener("resize", setup);
+
+setup();
 
 function draw(){
 
 ctx.fillStyle = "rgba(0,0,0,0.08)";
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle = "#ff2ea6";
+ctx.fillStyle = "#ff2bd6";
 ctx.font = fontSize + "px monospace";
 
 for(let i = 0; i < drops.length; i++){
@@ -42,7 +48,8 @@ drops[i] = 0;
 
 }
 
+requestAnimationFrame(draw);
+
 }
 
-setInterval(draw, 40);
-
+draw();
